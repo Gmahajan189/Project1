a1f364a7696ba3270cd11c304c6f6fbfe33f948d
Gmahajan189 <gmahajan189@gmail.com>
2024-03-09 14:41:46 +0530

    git commit -a

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 calculation = ""
 def add_to_calculation(symbol):
     global calculation
@@ -23,6 +24,7 @@ def clear_field():
     text_result.delete(1.0, "end")
     text_result.insert(1.0, calculation)
 root = tk.Tk()
+root.title("Python Calculator")
 root.geometry("300x275")
 text_result = tk.Text(root, height= 2, width= 16, font=("arial", 24))
 text_result.grid(columnspan=5)
@@ -46,15 +48,24 @@ btn8.grid(row=4,column=2)
 btn9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5)
 btn9.grid(row=4,column=3)
 btn0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5)
-btn0.grid(row=5,column=2)
+btn0.grid(row=5,column=1)
 btnplus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5)
 btnplus.grid(row=2,column=4)
 btnminus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5)
 btnminus.grid(row=3,column=4)
 btnmul = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5)
-btnmul.grid(row=4,column=4)
+btnmul.grid(row=5,column=4)
 btndiv = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5)
 btndiv.grid(row=4,column=4)
+btnopen = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5)
+btnopen.grid(row=5,column=2)
+btnclose = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5)
+btnclose.grid(row=5,column=3)
+btnclear = tk.Button(root, text="C", command= clear_field, width=5)
+btnclear.grid(row=6,column=1)
+btnequals = tk.Button(root, text="=", command= evaluate_calculation, width=5)
+btnequals.grid(row=6,column=4)
+
 
 
 root.mainloop()
